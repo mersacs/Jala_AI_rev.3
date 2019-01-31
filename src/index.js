05efe5ec6733f952b42ddd3bce49192b00b1c5d7
mersacs <mersacs.telkom@gmail.com>
2019-01-31 14:53:37 +0700

    update live demo

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 require( './styles/index.scss' );
 
+const navbar = ( ) => {
+      const nav = document.querySelector( '.navigation' );
+      const item = document.querySelectorAll('.navigation__link');
+      if ( document.documentElement.scrollTop || document.body.scrollTop > window.innerHeight ) {
+            nav.classList.add( "navigation--black" );
+            item.forEach((el)=> el.classList.add( "navigation__link--white" ));
+      } else {
+            nav.classList.remove( "navigation--black" );
+            item.forEach((el)=> el.classList.remove( "navigation__link--white" ));
+      }
+}
+
+window.addEventListener( "scroll", function ( e ) {
+      window.requestAnimationFrame( ( ) => {
+            navbar( );
+      } )
+} );
