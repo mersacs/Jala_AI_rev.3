6316b32a91b094b821331a2ca22e2e7b996d719f
mersacs <mersacs.telkom@gmail.com>
2019-02-05 09:06:22 +0700

    mugo last commit

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const reaction = (el) => {
             item.forEach((el)=> el.classList.add( "navigation__link--white" ));
             allSvg.forEach(el => el.style.stroke = "#ffffff");
       }
-} 
+};
 
 const navbar = ( ) => {    
       if ( document.documentElement.scrollTop || document.body.scrollTop > window.innerHeight ) {
@@ -25,12 +25,12 @@ const navbar = ( ) => {
       else {
             reaction("remove");
       }
-}
+};
 
 window.addEventListener( "scroll", function ( e ) {
       window.requestAnimationFrame( ( ) => {
             navbar( );
-      } )
+      } );
 } );
 
 const burger = document.querySelector(".navigation__burger");
@@ -41,7 +41,6 @@ arr.forEach(el => {
       el.addEventListener("click", () => {
             const container = document.querySelector(".navigation__container");
             
-
             if(burger.dataset.show === "0"){
                   container.classList.add("navigation__container--reveal");
                   popup.classList.add("navigation__popup--reveal");
@@ -57,7 +56,7 @@ arr.forEach(el => {
                         reaction("remove");
                   } 
             }
-      })
+      });
 });
 
 
@@ -76,4 +75,18 @@ const animate = ( ) => {
             line1.classList.remove( "line1-in" );
             line2.classList.remove( "line2-in" );
       }
-}
+};
+
+const features = document.querySelectorAll(".features__item");
+
+features.forEach(el => {
+      el.addEventListener("click", () => {
+            const type = el.parentNode.dataset.type;
+            const image = document.querySelectorAll(`.features__container--${type}-image img`);
+            const activeImage = document.querySelector(`.features__container--${type}-image .features__image__${type}--show`);
+
+            activeImage.classList.remove(`features__image__${type}--show`);
+            image[el.dataset.num - 1].classList.add(`features__image__${type}--show`);
+      });
+});
+
